20ef75d673fc0217a795001355534943f1d43b1d
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2025-02-16 23:48:17 +0300

    Create RAMBrands.java

---
+++ ComputerAccountingSystem/src/main/java/com/etu/labs/Enums/RAM/RAMBrands.java
@@ -0,0 +1,20 @@
+package com.etu.labs.Enums.RAM;
+
+public enum RAMBrands {
+    ACER("Acer"),
+    ADATA("ADATA"),
+    CRUCIAL("Crucial"),
+    HIKVISION("Hikvision"),
+    FOXLINE ("FoxLine"),
+    KINGSTON("Kingston"),
+    KINGSPEC("Kingspec"),
+    NETAC("Netac"),
+    PATRIOT("Patriot Memory"),
+    SAMSUNG("Samsung"),
+    QUMO("Qumo"),
+    TRANSCEND("Transcend");
+
+    private String name;
+
+    RAMBrands(String str){name = str;}
+}
