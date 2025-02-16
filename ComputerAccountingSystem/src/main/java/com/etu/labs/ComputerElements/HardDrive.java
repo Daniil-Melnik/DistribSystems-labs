41b279ac93d6d116c208d133ccc8d8c80ce273c5
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2025-02-16 15:08:53 +0300

    added annotations to HardDrive

--- ComputerAccountingSystem/src/main/java/com/etu/labs/ComputerElements/HardDrive.java
+++ ComputerAccountingSystem/src/main/java/com/etu/labs/ComputerElements/HardDrive.java
@@ -1,5 +1,90 @@
 package com.etu.labs.ComputerElements;
 
+import com.etu.labs.Enums.Disk.DiskBrands;
+import com.etu.labs.Enums.Disk.DiskConnector;
+import com.etu.labs.Enums.Disk.DiskType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
+@Table(name = "hard_drive")
 public class HardDrive {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "brand")
+    private DiskBrands brand;
+
+    @Column(name = "year_of_product")
+    private int yearOfProduct;
+
+    @Column(name = "capacity")
     private int capacity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private DiskType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "connector_type")
+    private DiskConnector connectorType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DiskBrands getBrand() {
+        return brand;
+    }
+
+    public void setBrand(DiskBrands brand) {
+        this.brand = brand;
+    }
+
+    public int getYearOfProduct() {
+        return yearOfProduct;
+    }
+
+    public void setYearOfProduct(int yearOfProduct) {
+        this.yearOfProduct = yearOfProduct;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public DiskType getType() {
+        return type;
+    }
+
+    public void setType(DiskType type) {
+        this.type = type;
+    }
+
+    public DiskConnector getConnectorType() {
+        return connectorType;
+    }
+
+    public void setConnectorType(DiskConnector connectorType) {
+        this.connectorType = connectorType;
+    }
 }
